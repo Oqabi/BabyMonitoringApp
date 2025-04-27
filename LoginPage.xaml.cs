f76c79ceda10b553ea2671203f8e413959678f5b
Ahmed AOqabi <ataha.aloqabi@gmail.com>
2025-04-27 14:05:45 +0200

    First desgin of the login screen. WIP

---
+++ LoginPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace BabyMonitoringApp
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnGoogleLoginClicked(object sender, TappedEventArgs e)
+        {
+            DisplayAlert("Login", "Google Login Clicked", "OK");
+        }
+
+        private void OnFacebookLoginClicked(object sender, TappedEventArgs e)
+        {
+            // TODO: Implement Facebook login logic
+            DisplayAlert("Login", "Facebook Login Clicked", "OK");
+        }
+    }
+
+}
