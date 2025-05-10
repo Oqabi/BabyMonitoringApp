01cf6f18e624c6ed19ad4229230531abfd6a7842
Ahmed AOqabi <ataha.aloqabi@gmail.com>
2025-05-10 20:03:20 +0200

    PS : [19fc16dd].

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -15,6 +15,7 @@ namespace BabyMonitoringApp
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Roboto-Regular.ttf", "Roboto");
                 });
 
 #if DEBUG
